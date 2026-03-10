7e6f603ff79201327c4ec950b24998347bd4fcb5
fardeenib <fardeenib9686@gmail.com>
2026-03-10 16:18:12 +0000

    final fix for autograder

--- Assignment1.java
+++ Assignment1.java
@@ -1,36 +1,42 @@
 class Assignment1 {
-  public static int[] findLeapYears(int year) {
-    int[] leapYears = new int[15];
-    int count = 0;
-    int currentYear = year;
 
-    while (count < 15) {
-      if (isLeapYear(currentYear)) {
-        leapYears[count] = currentYear;
-        count++;
-      }
-      currentYear++;
-    }
-    return leapYears;
-  }
+    public static int[] findLeapYears(int year) {
+        int[] leapYears = new int[15];
+        int count = 0;
+        int currentYear = year;
 
-  private static boolean isLeapYear(int year) {
-    if (year % 400 == 0) {
-      return true;
-    } else if (year % 4 == 0 && year % 100 != 0) {
-      return true;
-    } else {
-      return false;
+        while (count < 15) {
+            if (isLeapYear(currentYear)) {
+                leapYears[count] = currentYear;
+                count++;
+            }
+            currentYear++;
+        }
+        return leapYears;
     }
-  }
 
-  public static void main(String[] args) {
-    int year = 2000;
-    int[] leapYears;
-    leapYears = findLeapYears(year);
-    for (int index = 0; index < leapYears.length; index++) {
-      System.out.print(leapYears[index] + " ");
+    private static boolean isLeapYear(int year) {
+        if (year % 400 == 0) {
+            return true;
+        } 
+        else if (year % 4 == 0 && year % 100 != 0) {
+            return true;
+        } 
+        else {
+            return false;
+        }
     }
-    System.out.println(); // For a new line after output
-  }
+
+    public static void main(String[] args) {
+        int year = 2000;
+        int[] leapYears = findLeapYears(year);
+
+        for (int i = 0; i < leapYears.length; i++) {
+            System.out.print(leapYears[i]);
+            if (i < leapYears.length - 1) {
+                System.out.print(" ");
+            }
+        }
+        System.out.println();
+    }
 }
